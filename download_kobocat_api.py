ba9011036517468355ee56cc6932e8316f8fab78
Andre Daniel Cal <andredanielcal@gmail.com>
2022-07-15 16:11:13 +0800

    added URL counter and Status code displays

--- download_kobocat_api.py
+++ download_kobocat_api.py
@@ -2,12 +2,19 @@ import pandas
 import requests
 import os 
 
+
 #Download list of forms in server
 def download_form_all(token):
-    print("downloading forms list")
+    print("Status Codes")
+    print("200 - Successful")
+    print("201 - Resource successfully created")
+    print("204 - Resource successfully deleted")
+    print("403 - Permission denied to resource")
+    print("404 - Resource was not found")
     down_form = requests.get('https://kc.humanitarianresponse.info/api/v1/forms.csv', headers={'Authorization': 'Token {}'.format(token)})
-    open('download_all.csv','wb').write(down_form.content)
-    print("downloading complete")
+    print("Downloading Forms List | Status Code: {}".format(down_form))
+    open('Download_all.csv','wb').write(down_form.content)
+    print("Downloading complete!")
 
 #get all urls from the list
 def read():
@@ -17,7 +24,7 @@ def read():
     for url,formid,description in zip(df.url,df.formid,df.description):
         csv_file.append([url,formid,description])
         i = i + 1
-    return csv_file
+    return csv_file, i
 
 #Iterate over each url and download, also output to folder 
 def download_form_list(links,token):
@@ -25,15 +32,16 @@ def download_form_list(links,token):
     if not os.path.exists('COLLABdev'):
         os.makedirs('COLLABdev')
     for link in links:
-        print("Downloading {} form".format(link[2]))
         down_form = requests.get(link[0], headers={'Authorization': 'Token {}'.format(token)})
+        print("Downloading {} Form | Status Code {}".format(link[2],down_form))
         open('COLLABdev/COLLABDev_{}_{}.csv'.format('PLACEHOLDER',link[2]),'wb').write(down_form.content)
-    print("All forms downloaded")
+    print("All Forms Downloaded")
 
 token = "" # insert kobotoolbox access token
 if not token:
     print("No token")
 else:
     download_form_all(token)
-    links = read()
+    links,i = read()
+    print("URL List count: {}".format(i))
     download_form_list(links,token)
